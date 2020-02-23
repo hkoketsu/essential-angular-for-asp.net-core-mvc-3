c74e745c83e300c0cb237d3b522d924916e84f70
hkoketsu <hiroki.koketsu@gmail.com>
2020-02-23 09:46:35 -0800

    Create data models in client and server sides

---
+++ SportsStore/ServerApp/Models/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServerApp.Models
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Product> Products { get; set; }
+
+		public DbSet<Supplier> Supplier { get; set; }
+		public DbSet<Rating> Ratings { get; set; }
+	}
+}
