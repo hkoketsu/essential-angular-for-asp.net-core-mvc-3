1f1600974576f8880c33199ca7acc68f55a0e74b
hkoketsu <hiroki.koketsu@gmail.com>
2020-02-23 11:07:26 -0800

    Deal with circularr eference

--- SportsStore/ServerApp/Controllers/ProductValuesController.cs
+++ SportsStore/ServerApp/Controllers/ProductValuesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ServerApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ServerApp.Controllers
 {
@@ -20,6 +21,37 @@ namespace ServerApp.Controllers
 		}
 
 		[HttpGet("{id}")]
-		public Product GetProduct(long id) => context.Products.Find(id);
+		public Product GetProduct(long id)
+		{
+			//System.Threading.Thread.Sleep(5000);
+			Product result = context.Products
+					.Include(p => p.Supplier).ThenInclude(s => s.Products)
+					.Include(p => p.Ratings)
+					.FirstOrDefault(p => p.ProductId == id);
+			if (result != null)
+			{
+				if (result.Supplier != null)
+				{
+					result.Supplier.Products = result.Supplier.Products.Select(p =>
+						new Product
+						{
+							ProductId = p.ProductId,
+							Name = p.Name,
+							Category = p.Category,
+							Description = p.Description,
+							Price = p.Price,
+						});
+				}
+
+				if (result.Ratings != null)
+				{
+					foreach (Rating r in result.Ratings)
+					{
+						r.Product = null;
+					}
+				}
+			}
+			return result;
+		}
 	}
 }
